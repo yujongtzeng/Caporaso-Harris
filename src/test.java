3a83860c1722b145af58e52b7cec418d96933bee
yujongtzeng <yujong@gmail.com>
2020-05-21 15:05:03 +0800

    work on tests

--- src/test.java
+++ src/test.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
 import java.io.FileNotFoundException; 
 import java.io.IOException; 
 /**
- * The Test class runs pre-designed tests for CH and F0Table classes.
+ * The Test class runs pre-designed tests for CH and F0table classes.
  * It compares files in testFiles directory with program outputs line 
  * by line. 
  * If there are mismatchs, the difference will be printed out.
+ * <p>
+ * It will run tests for CH class with (deg, maxNode) = (6, 15), (10, 4) and 
+ * tests for F0table class for (a, b, maxNode) = (10, 1, 3), (4, 2, 3), 
+ * (6, 3, 7) and (4, 4, 10). 
+ * This tests covers all numbers previously compute in Caporaso-Harris' paper
+ * "Counting plane curves of any genus" p.5-6 and in Vakil's paper "Counting 
+ * curves on rational surfaces" p.75 for P^1*P^1.
+
  * @author Yu-jong Tzeng
- * @version 1.0
- * @since May 19, 2020.
+ * @version 2.0
+ * @since May 21, 2020.
  */
 public class Test
 {   
@@ -22,13 +30,13 @@ public class Test
         // testFiles/ first.
         testCH(6, 15);     //default (10, 4, 1)
         testCH(10, 4);     //default (6, 15, 2)
-        //testF0(10, 1, 3, 1);    //default (10, 1, 3, 1)
-        //testF0(4, 2, 3, 2);     //default(4, 2, 3, 2)
-        //testF0(6, 3, 7, 3);     //default (6, 3, 7, 3)
-        //testF0(4, 4, 10, 4);    //default(4, 4, 10, 4)
+        testF0(10, 1, 3);    //default (10, 1, 3, 1)
+        testF0(4, 2, 3);     //default(4, 2, 3, 2)
+        testF0(6, 3, 7);     //default (6, 3, 7, 3)
+        testF0(4, 4, 10);    //default(4, 4, 10, 4)
     } 
     /**
-     * Outlne the comparison between files.
+     * Output the comparison between test files and output files of CH.
      */
     private static void testCH(int maxdeg, int delta) {
         CH ch = new CH(maxdeg, delta);
@@ -50,19 +58,30 @@ public class Test
         }
         System.out.println("Done");
     }
-    /*private static void testF0(int a, int b, int delta, int testN) {
-        F0table f0 = new F0table(a, b, delta);
+    /**
+     * Output the comparison between test files and output files of F0table.
+     */
+    private static void testF0(int a, int b, int maxNode) {
+        F0table f0 = new F0table(a, b, maxNode);
+        f0.printLast = a + 1;
         f0.compute();     
+        System.out.format("Testing bidegree (%d, %d) and maxNode = %d:\n", 
+            a, b, maxNode);
         for (int i = 0; i <= a; i++) {
-            for (int r = 0; r <= delta; r++) {
+            for (int r = 0; r <= maxNode; r++) {
                 int g = MyF.g_a(i, b) - r;
-                String fname = "O("+ i + ", " + b + ")_g=" + g + ".txt";
-                System.out.println(fname);
-                compare(new File("test/F0" + testN +"/" + fname), 
-                new File("output/F0/" + fname));
+                String fname = String.format("O(%d, %d)_g=%d.txt", i, b, g);
+                //System.out.println(fname);
+                String dir = String.format("F0(%d, %d, %d)/", a, b, maxNode);
+                Boolean diffL = compare(new File("testFiles/" +  dir 
+                    + fname), new File("../output/F0/" + fname));
+                if (diffL) {
+                    System.out.println(" for " + fname);
+                }  
             }
         }
-    }*/  
+        System.out.println("Done");
+    }
     /**
      * Print the difference of two files. Return True if the two files
      * have different number of lines. 
